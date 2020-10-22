dcbef453c1f9e9fcea7b34b05ee2b78ad791a110
jay1260 <jay1260@naver.com>
2020-10-22 15:43:41 +0900

    add_list_jsp

--- src/main/java/com/it/gb4/board/notice/NoticeController.java
+++ src/main/java/com/it/gb4/board/notice/NoticeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,13 +18,40 @@ public class NoticeController {
 	@Autowired
 	private NoticeService noticeService;
 	
+	@PostMapping("noticeWrite")
+	public ModelAndView setInsert(BoardDTO boardDTO)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		int result = noticeService.setInsert(boardDTO);
+		
+		String message = "Write Fail";
+		if(result>0) {
+			message = "Write Success";
+		}
+		mv.addObject("msg", message);
+		mv.addObject("path", "./noticeList");
+		
+		mv.setViewName("common/result");
+		
+		return mv;
+	}
+	
+	@GetMapping("noticeWrite")
+	public ModelAndView setInsert()throws Exception{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("board/boardWrite");
+		mv.addObject("board", "notice");
+		
+		return mv;
+	}
+	
 	//@RequestMapping(value = "noticeList")
 	@GetMapping("noticeList")
 	public ModelAndView getList(Pager pager)throws Exception{
 		ModelAndView mv = new ModelAndView();
 		List<BoardDTO> ar = noticeService.getList(pager);
 		
-		mv.addObject("lists", ar);
+		mv.addObject("board", "notice");
+		mv.addObject("list", ar);
 		mv.addObject("pager", pager);
 		System.out.println("Notice List");
 		mv.setViewName("board/boardList");
--- src/main/java/com/it/gb4/board/qna/QnaController.java
+++ src/main/java/com/it/gb4/board/qna/QnaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,13 +18,42 @@ public class QnaController {
 	@Autowired
 	private QnaService qnaService;
 	
+	@PostMapping("qnaWrite")
+	public ModelAndView setInsert(BoardDTO boardDTO)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		int result = qnaService.setInsert(boardDTO);
+		
+		String message = "Write Fail";
+		if(result>0) {
+			message = "Write Success";
+		}
+		mv.addObject("msg", message);
+		mv.addObject("path", "./qnaList");
+		
+		mv.setViewName("common/result");
+		
+		return mv;
+	}
+	
+	@GetMapping("qnaWrite")
+	public ModelAndView setInsert() throws Exception{
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("board/boardWrite");
+		mv.addObject("board", "qna");
+		return mv;
+	}
+	
 	@GetMapping("qnaList")
 	public ModelAndView getList(Pager pager) throws Exception {
-		List<BoardDTO> ar = qnaService.getList(pager);
-		
 		ModelAndView mv = new ModelAndView();
+		List<BoardDTO> ar = qnaService.getList(pager);
+
+		BoardDTO boardDTO = ar.get(0);
+		QnaDTO qnaDTO = (QnaDTO)boardDTO;
+		System.out.println(qnaDTO.getDepth());
 		
-		mv.addObject("lists", ar);
+		mv.addObject("board", "qna");
+		mv.addObject("list", ar);
 		mv.addObject("pager", pager);
 		mv.setViewName("board/boardList");
 		
