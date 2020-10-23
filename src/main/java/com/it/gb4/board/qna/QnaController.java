15723378e514084b284bfd626ac18a1921b5e92d
jay1260 <jay1260@naver.com>
2020-10-23 09:20:36 +0900

    error_fix

--- src/main/java/com/it/gb4/board/qna/QnaController.java
+++ src/main/java/com/it/gb4/board/qna/QnaController.java
@@ -60,10 +60,8 @@ public class QnaController {
 	public ModelAndView setUpdate() throws Exception {
 		// 글번호 출력
 		// 글제목, 글내용
-		BoardDTO boardDTO = new BoardDTO();
 		ModelAndView mv = new ModelAndView();
 		
-		mv.addObject("dto", boardDTO);
 		mv.setViewName("board/boardUpdate");
 		
 		mv.addObject("board", "qna");
